3d937693ef8a41e5d688e999f7a3da7300ddfba4
kaushaldeokar <91072847+kaushaldeokar@users.noreply.github.com>
2022-08-19 23:12:39 +0530

    Update pascal_triangle.cpp

--- Day 3/pascal_triangle.cpp
+++ Day 3/pascal_triangle.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>>ans;
+        ans.push_back({1});
+        // ans.push_back({1,1});
+        for(int i=1;i<n;i++){
+            vector<int> v;
+            v.push_back(1);
+            for(int j=1;j<i;j++){
+                v.push_back(ans[i-1][j]+ans[i-1][j-1]);
+            }
+            v.push_back(1);
+            ans.push_back(v);
+        }
+        return ans;
+        
+    }
+};
